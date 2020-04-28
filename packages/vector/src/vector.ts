e6cdc885bfbc56e9ba296d478661854817a29e59
Tim Severien <timseverien@gmail.com>
2020-04-29 00:28:52 +0200

    Add getMagnitudeSquared to vector bundle

--- packages/vector/src/vector.ts
+++ packages/vector/src/vector.ts
@@ -1,5 +1,6 @@
 export { default as add } from './add';
 export { default as componentWise } from './component-wise';
 export { default as divide } from './divide';
+export { default as getMagnitudeSquared } from './get-magnitude-squared';
 export { default as multiply } from './multiply';
 export { default as subtract } from './subtract';
