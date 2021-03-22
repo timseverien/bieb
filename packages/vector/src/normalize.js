0a19a79a97909c30eaba2424ba9220e7a052cdfa
Tim Severien <timseverien@gmail.com>
2021-03-22 14:26:18 +0100

    Fix normalization for zero-length vectors

--- packages/vector/src/normalize.js
+++ packages/vector/src/normalize.js
@@ -1,7 +1,14 @@
+import assertVector from './assertVector';
 import divideScalar from './divideScalar';
 import getMagnitude from './getMagnitude';
 
 export default function normalize(v) {
+	assertVector(v);
+
+	if (v.every((c) => c === 0)) {
+		return new Array(v.length).fill(0);
+	}
+
 	const magnitude = getMagnitude(v);
 
 	return divideScalar(v, magnitude);
--- packages/vector/src/normalize.spec.js
+++ packages/vector/src/normalize.spec.js
@@ -12,6 +12,17 @@ describe('normalize', () => {
 		expect(result).toMatchObject([1, 0]);
 	});
 
+	test('given a zero-length vector returns a vector of [0, 0]', () => {
+		// Arrange
+		const vector = [0, 0];
+
+		// Act
+		const result = normalize(vector);
+
+		// Assert
+		expect(result).toMatchObject([0, 0]);
+	});
+
 	test.each([
 		['[1, 2, 3]'],
 		[{}],
